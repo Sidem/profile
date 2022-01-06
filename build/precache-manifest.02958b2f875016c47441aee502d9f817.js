09f45c338a2ef231467477c7328677ea36e1d780
Sidem <7662187+Sidem@users.noreply.github.com>
2022-01-06 12:58:13 +0100

    initial build

---
+++ build/precache-manifest.02958b2f875016c47441aee502d9f817.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4098b594c8f677f155e8889929e0350",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e7010385106c388ee2d",
+    "url": "/static/js/2.247fc507.chunk.js"
+  },
+  {
+    "revision": "689b94b897cc6d07509b955cdc774cc7",
+    "url": "/static/js/2.247fc507.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a620e1fd9f5c04b835b6",
+    "url": "/static/js/main.17c6548e.chunk.js"
+  },
+  {
+    "revision": "84abe5e08f01abcdcab9",
+    "url": "/static/js/runtime-main.12cb5a08.js"
+  }
+]);
